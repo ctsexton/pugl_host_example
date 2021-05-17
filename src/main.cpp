a995597b50985979743dd7e4355cce6a8b36d734
Cameron <cameron.t.sexton@gmail.com>
2021-05-16 17:33:12 -0700

    get it working

--- src/main.cpp
+++ src/main.cpp
@@ -30,10 +30,12 @@ int main(int argc, char** argv) {
     std::cout << "Usage: ./PuglHost <plugin_uri> OR ./PuglHost DUMMY_CHILD" << std::endl;
     return 0;
   }
-  auto SELECTED_PLUGIN_URI = std::string(argv[1]);
+  auto first_arg = std::string(argv[1]);
+  Lilv::World lilv_world;
+  lilv_world.load_all();
+  Lilv::Plugins plugins = lilv_world.get_all_plugins();
 
-  if (SELECTED_PLUGIN_URI == std::string("DUMMY_CHILD")) {
-    // Demonstrates embedding a child PuglView in the parent
+  if (first_arg == std::string("DUMMY_CHILD")) {
     pugl::World pugl_world {pugl::WorldType::program};
     PuglRect parent_frame = {0, 0, 512, 512};
     PuglRect child_frame = {20, 20, 472, 472};
@@ -47,14 +49,24 @@ int main(int argc, char** argv) {
       pugl_world.update(0.1);
     }
     return 0;
+  } else if (first_arg == std::string("--list-all-plugins")) {
+    LilvIter* pli = plugins.begin();
+    while(!plugins.is_end(pli)) {
+      Lilv::Plugin plg = plugins.get(pli);
+      std::cout << plg.get_uri().as_string() << std::endl;
+      pli = plugins.next(pli);
+    }
+    return 0;
   }
+  auto SELECTED_PLUGIN_URI = first_arg;
 
-  Lilv::World lilv_world;
-  lilv_world.load_all();
   Lilv::Node plugin_uri = lilv_world.new_uri(SELECTED_PLUGIN_URI.c_str());
-  Lilv::Plugins plugins = lilv_world.get_all_plugins();
   Lilv::Plugin plugin = plugins.get_by_uri(plugin_uri);
   Lilv::UIs plugin_uis = plugin.get_uis();
+  if (plugin_uis.begin() == nullptr) {
+    std::cout << "Plugin does not have a UI\n";
+    return 0;
+  }
   Lilv::UI first_ui = plugin_uis.get(plugin_uis.begin());
   Lilv::Node ui_uri = first_ui.get_uri();
   Lilv::Node ui_bundle_uri = first_ui.get_bundle_uri();
@@ -73,8 +85,6 @@ int main(int argc, char** argv) {
   ParentView view{pugl_world, parent_frame};
   view.show();
 
-  auto parent = view.nativeWindow();
-
   LV2_URID_Map urid_map {
     NULL,
     urid_map_fn
@@ -83,7 +93,7 @@ int main(int argc, char** argv) {
     LV2_URID__map, &urid_map
   };
   const LV2_Feature parent_feature = {
-    LV2_UI__parent, &parent
+    LV2_UI__parent, (void*)view.nativeWindow()
   };
   const LV2_Feature idle_feature = {
     LV2_UI__idleInterface, NULL
@@ -107,12 +117,11 @@ int main(int argc, char** argv) {
     ui_features
   );
 
-  // What am I missing here?
-  // How do I get the instance view
-  // to draw inside the parent?
+  const LV2UI_Idle_Interface* idle_iface = (const LV2UI_Idle_Interface*)suil_instance_extension_data(instance, LV2_UI__idleInterface);
 
   while (!view.quit()) {
-    pugl_world.update(0.1);
+    pugl_world.update(1.0 / 30.0);
+    idle_iface->idle(suil_instance_get_handle(instance));
   }
 
   return 0;
